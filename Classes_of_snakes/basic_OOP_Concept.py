9011588e3fad15ff71a4db635deb1726e31e4b99
emeka <chukwujapheth232@gmail.com>
2022-08-27 12:23:16 +0300

    .

--- Classes_of_snakes/basic_OOP_Concept.py
+++ Classes_of_snakes/basic_OOP_Concept.py
@@ -8,14 +8,21 @@ class Human:
     self.stomach = None
     self.bowel = {"stool":0}
     self.bladder = {"piss":0}
-    self.legs = {"speed":10}
-    self.race = None
-    self.gender = None
-    self.age = None
+    self.legs = {"speed":10,"condition":"Good"}
+    self.race = "African"
+    self.gender = "Female"
+    self.age = 1
+    self.car = {"car":"Bugatti","color":"black"}
 
   def eat(self,food):
-    self.stomach = f"Food"
-    self.bowel += 1 
+    self.stomach = f"{food}"
+    self.bowel[u"stool"] += 1 
+    return(f"Thank you for the {food} it was good")
+
+  def drink(self,drink):
+    self.stomach = f"{drink}"
+    self.bladder[u"piss"] = 1 
+    return f"Drank {drink}"
 
   def stool(self):
     if self.bowel[u"stool"] <= 0:
@@ -23,22 +30,62 @@ class Human:
     else:
         self.bowel[u"stool"] = 0
         self.stomach = None
+        return "Thant Stinks, Eww"
 
   def piss(self):
     if not self.bladder[u"piss"] ==  'empty':
         self.bladder[u"piss"] = "empty"
+        return "I was actually pressed, I needed to use the bathroom"
     else:
         return "Bladder is empty"
  
   def grow(self, years):
     self.age += years
+    return f"{years} years later, I am now {self.age} years old"
 
   def How_are_you(self):
-    state = None
+    state = ""
     if self.stomach != "Full":
         state = state + "I am Hungry"
     else: 
-        return "I am Fine"
+        state = state + "I am Fine"
+    return state
+
+  def what_color_is_your_car(self):
+    color = self.car[u"color"]
+    car = self.car[u"car"]
+    return f"Mine is {color}, What about You, What Color is your {car} "
+
+
+  def __repr__(self) -> str:
+     return f"Breaths Air: I am Created my age is {self.age}, I have a bugatti and I am strong \nGet Creative with me"
+
+
+# Andrew was created
+Andrew = Human()
+print(Andrew)
+print("The Top G is born")
+
+# We his creator asked him
+print(Andrew.How_are_you())
+
+# we gave him some food because he said he is hungry
+print(Andrew.eat("Corn Flakes"))
+
+# we gave him something to drink too 
+print(Andrew.drink("Sparkling Water"))
+
+# we want him to take a piss
+print(Andrew.piss())
+
+# we want him to take a stool
+print(Andrew.stool())
+
+# we want to make andrew grow older
+print(Andrew.grow(10))
+
+print(Andrew.what_color_is_your_car())
+
 
 
   
