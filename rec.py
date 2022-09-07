fb8b4a95d4e0f53bb12afb4eff031e9d77a66d51
emeka <chukwujapheth232@gmail.com>
2022-09-07 08:51:49 +0300

    .

--- rec.py
+++ rec.py
@@ -1,1 +1,19 @@
-lists   =   [[],5,4,5,[],0] # a nested list
+def closestNumbers(numbers):
+    if numbers:
+    # Write your code here
+       numbers.sort()
+       sorted_num = numbers
+        
+       seen = []
+       min_diff = abs(sorted_num[0] - sorted_num[1])
+       for i in range(len(numbers)-1):
+          if abs(sorted_num[i] - sorted_num[i+1]) == min_diff :
+            small = sorted_num[i]
+            large =  sorted_num[i+1]
+            seen.append([small,large])
+            
+       for i in seen:
+         print(i[0],i[1])
+
+
+closestNumbers([1,2,3,4,5,7,10,9])
